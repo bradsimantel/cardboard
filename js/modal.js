23737fcaa6626ae8c60189759678f8945e8daa00
Brad Simantel <brad@ottter.com>
2024-09-24 20:44:58 -0700

    Update color modal

--- js/modal.js
+++ js/modal.js
@@ -5,23 +5,22 @@ import {
 import CloseIcon from "./icons/close.js";
 import ToneIcon from "./icons/tone.js";
 
+function handleUpdateColors(backgroundColor, foregroundColor) {
+  document.documentElement.style.setProperty(
+    "--background-color",
+    backgroundColor
+  );
+  document.documentElement.style.setProperty(
+    "--foreground-color",
+    foregroundColor
+  );
+}
+
 export default function Modal({ isOpen, onClose }) {
   const [backgroundColor, setBackgroundColor] = useState("#ffffff");
   const [foregroundColor, setForegroundColor] = useState("#000000");
 
   if (!isOpen) return null;
-
-  const handleUpdateColors = () => {
-    document.documentElement.style.setProperty(
-      "--background-color",
-      backgroundColor
-    );
-    document.documentElement.style.setProperty(
-      "--foreground-color",
-      foregroundColor
-    );
-    onClose();
-  };
 
   return html`
     <div
@@ -47,35 +46,100 @@ export default function Modal({ isOpen, onClose }) {
             </div>
             <${CloseIcon} />
           </div>
-          <div class="p-3">
-            <form>
-              <div class="mb-3">
-                <label for="backgroundColor" className="d-block mb-1">
-                  Background Color
-                </label>
-                <input
-                  type="text"
-                  class="d-block bg-white rounded border w-100 p-1 px-2"
-                  id="backgroundColor"
-                  name="backgroundColor"
-                  value=${backgroundColor}
-                  onInput=${(e) => setBackgroundColor(e.target.value)}
-                />
+          <form class="flex">
+            <div class="w-50 p-3 pe-2">
+              <label for="backgroundColor" className="d-block mb-1">
+                Background
+              </label>
+              <input
+                type="text"
+                class="d-block bg-white rounded border w-100 p-1 px-2"
+                id="backgroundColor"
+                name="backgroundColor"
+                value=${backgroundColor}
+                onInput=${(e) => setBackgroundColor(e.target.value)}
+              />
+            </div>
+            <div class="w-50 p-3 ps-2">
+              <label for="foregroundColor" className="d-block mb-1">
+                Foreground
+              </label>
+              <input
+                type="text"
+                class="d-block bg-white rounded border w-100 p-1 px-2"
+                id="foregroundColor"
+                name="foregroundColor"
+                value=${foregroundColor}
+                onInput=${(e) => setForegroundColor(e.target.value)}
+              />
+            </div>
+          </form>
+          <div class="p-3 pt-0 mb-2">
+            <span class="d-block mb-1">Suggested Combinations</span>
+            <div class="flex mb-2">
+              <div
+                class="w-33 text-center p-2 rounded border"
+                onClick=${() => {
+                  setBackgroundColor("#ffffff");
+                  setForegroundColor("#000000");
+                  handleUpdateColors("#ffffff", "#000000");
+                }}
+              >
+                White/Black
               </div>
-              <div class="mb-2">
-                <label for="foregroundColor" className="d-block mb-1">
-                  Foreground Color
-                </label>
-                <input
-                  type="text"
-                  class="d-block bg-white rounded border w-100 p-1 px-2"
-                  id="foregroundColor"
-                  name="foregroundColor"
-                  value=${foregroundColor}
-                  onInput=${(e) => setForegroundColor(e.target.value)}
-                />
+              <div
+                class="w-33 text-center p-2 rounded border ms-2"
+                onClick=${() => {
+                  setBackgroundColor("#000000");
+                  setForegroundColor("#ffffff");
+                  handleUpdateColors("#000000", "#ffffff");
+                }}
+              >
+                Black/White
               </div>
-            </form>
+              <div
+                class="w-33 text-center p-2 rounded border ms-2"
+                onClick=${() => {
+                  setBackgroundColor("#000080");
+                  setForegroundColor("#b4cde2");
+                  handleUpdateColors("#000080", "#b4cde2");
+                }}
+              >
+                Blue/Blue
+              </div>
+            </div>
+            <div class="flex">
+              <div
+                class="w-33 text-center p-2 rounded border"
+                onClick=${() => {
+                  setBackgroundColor("#5f4b8b");
+                  setForegroundColor("#e69a8d");
+                  handleUpdateColors("#5f4b8b", "#e69a8d");
+                }}
+              >
+                Purple/Peach
+              </div>
+              <div
+                class="w-33 text-center p-2 rounded border ms-2"
+                onClick=${() => {
+                  setBackgroundColor("#000000");
+                  setForegroundColor("#1DB954");
+                  handleUpdateColors("#000000", "#1DB954");
+                }}
+              >
+                Black/Green
+              </div>
+              <div
+                class="w-33 text-center p-2 rounded border ms-2"
+                onClick=${() => {
+                  setBackgroundColor("#f0e9eb");
+                  setForegroundColor("#7a0b45");
+                  handleUpdateColors("#f0e9eb", "#7a0b45");
+                }}
+              >
+                Cream/Maroon
+              </div>
+            </div>
           </div>
           <div class="flex justify-content-between p-3 border-top">
             <span class="border rounded px-3 py-1" onClick=${onClose}>
@@ -83,7 +147,10 @@ export default function Modal({ isOpen, onClose }) {
             </span>
             <span
               class="bg-black rounded px-3 py-1"
-              onClick=${handleUpdateColors}
+              onClick=${() => {
+                handleUpdateColors(backgroundColor, foregroundColor);
+                onClose();
+              }}
             >
               Update Colors
             </span>
