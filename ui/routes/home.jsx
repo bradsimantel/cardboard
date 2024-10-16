66399f51697019c0d280534772e02568b1e6a0d2
Brad Simantel <brad@ottter.com>
2024-10-16 15:22:25 -0700

    Put some text on the page

--- ui/routes/home.jsx
+++ ui/routes/home.jsx
@@ -55,11 +55,24 @@ export default function Home() {
 
   return (
     <>
-      <div style={`filter: url(${svgDataUrl}#color-shift);`}>
+      <div
+        style={`filter: url(${svgDataUrl}#color-shift); position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;`}
+      >
         <div style='background: url("data:image/webp;base64,UklGRjAAAABXRUJQVlA4TCQAAAAvA8AAAIVS27ahb3uzOUmSJoqqrOq6P47of8DQdcprYdP8/VY="); filter: contrast(20000)'>
           <div style="mix-blend-mode: soft-light; filter: grayscale(100%);">
             <div style="height: 100%; background-image: url(/beach.jpg); background-size: cover; background-position: center;" />
           </div>
+        </div>
+      </div>
+      <div style="position: absolute; z-index: 1; height: 100%; width: 100%;">
+        <div style="height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+          <h1 style="font-size: 5rem; color: #cfe2ff; padding: 0 1rem; background-color: #052c65;">
+            Organize anything together
+          </h1>
+          <h2 style="font-size: 2rem; color: #cfe2ff; padding: 0 1rem; background-color: #052c65;">
+            Peachtree is the simplest way to keep track of everything in your
+            life
+          </h2>
         </div>
       </div>
     </>
