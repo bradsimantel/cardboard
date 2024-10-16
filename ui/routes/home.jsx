e431d7f5bb4fe3fd2a8cc0b48c2bfb7ad63e5f83
Brad Simantel <brad@ottter.com>
2024-10-16 15:49:47 -0700

    Use Tailwind

--- ui/routes/home.jsx
+++ ui/routes/home.jsx
@@ -56,20 +56,24 @@ export default function Home() {
   return (
     <>
       <div
-        style={`filter: url(${svgDataUrl}#color-shift); position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;`}
+        className="absolute top-0 left-0 w-full h-full -z-10"
+        style={{ filter: `url(${svgDataUrl}#color-shift)` }}
       >
-        <div style='background: url("data:image/webp;base64,UklGRjAAAABXRUJQVlA4TCQAAAAvA8AAAIVS27ahb3uzOUmSJoqqrOq6P47of8DQdcprYdP8/VY="); filter: contrast(20000)'>
-          <div style="mix-blend-mode: soft-light; filter: grayscale(100%);">
-            <div style="height: 100%; background-image: url(/beach.jpg); background-size: cover; background-position: center;" />
+        <div className="bg-[url('data:image/webp;base64,UklGRjAAAABXRUJQVlA4TCQAAAAvA8AAAIVS27ahb3uzOUmSJoqqrOq6P47of8DQdcprYdP8/VY=')] contrast-[20000%]">
+          <div className="mix-blend-soft-light grayscale">
+            <div
+              className="h-full bg-cover bg-center"
+              style={{ backgroundImage: "url(/beach.jpg)" }}
+            />
           </div>
         </div>
       </div>
-      <div style="position: absolute; z-index: 1; height: 100%; width: 100%;">
-        <div style="height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;">
-          <h1 style="font-size: 5rem; color: #cfe2ff; padding: 0 1rem; background-color: #052c65;">
+      <div className="absolute z-10 h-full w-full">
+        <div className="h-full flex flex-col justify-center items-center">
+          <h1 className="text-5xl text-[#cfe2ff] p-4 bg-[#052c65]">
             Organize anything together
           </h1>
-          <h2 style="font-size: 2rem; color: #cfe2ff; padding: 0 1rem; background-color: #052c65;">
+          <h2 className="text-2xl text-[#cfe2ff] p-4 bg-[#052c65]">
             Peachtree is the simplest way to keep track of everything in your
             life
           </h2>
